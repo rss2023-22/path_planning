8c20616aafe87a14036b63f53312256a367e1650
brrich <brrich@mit.edu>
2023-04-13 20:15:05 +0000

    cleaned up path planning code

--- src/path_planning.py
+++ src/path_planning.py
@@ -70,8 +70,7 @@ class PathPlan(object):
 
     def map_cb(self, msg):
         self.map_res = msg.info.resolution
-        map = np.array(list(msg.data))
-        map = np.reshape(map,(msg.info.height, msg.info.width)) # convert from row-major order
+        map = np.reshape(np.array(list(msg.data)),(msg.info.height, msg.info.width)) # convert from row-major order
         
         height = np.shape(map)[0]; self.height = height
         width = np.shape(map)[1]; self.width = width
@@ -82,20 +81,19 @@ class PathPlan(object):
         rot_matrix[0][3] = msg.info.origin.position.x; rot_matrix[1][3] = msg.info.origin.position.y; rot_matrix[2][3] = msg.info.origin.position.z
         rot_alt = [rot_matrix[0][0:3],rot_matrix[1][0:3],rot_matrix[2][0:3]]
         self.rot_matrix = rot_matrix; self.rot_alt = rot_alt; self.rot_back = np.linalg.inv(self.rot_matrix); self.rot_back_alt = np.linalg.inv(self.rot_alt)
-        self.map = map
-        map[map > 0] = 1
-        map[map < 0] = 1
+
+        # dilate
+        map[map > 0] = 1; map[map < 0] = 1
         map = map.astype('uint8')
         kernel = np.ones((12, 12), np.uint8)
         map = ndimage.binary_dilation(map,structure=kernel)
         self.map = map
-        print('MAP INITIATED')
-        print('-----------------------------------')
+        print('### MAP INITIATED ###')
 
     def initial_pose_cb(self,data):
         u,v = self.mapToPixelCoords(data.pose.pose.position.x,data.pose.pose.position.y)
         if  (u,v) != self.start_pos:
-            print('new start(init)' + str((u,v)))
+            print('### new start(init)' + str((u,v))+' ### ')
             self.start_pos = (u,v)
 
     def odom_cb(self, data): # sets the curren position of the car... currently green arrow doesn't work?? same fix as in last lab tho
@@ -112,67 +110,14 @@ class PathPlan(object):
     def goal_cb(self, data):
         theta = 2*np.arctan2(data.pose.orientation.z,data.pose.orientation.w)
         self.goal_pos_irl = (data.pose.position.x,data.pose.position.y,theta)
-        print('set new goal:'+str(self.goal_pos_irl))
-        print('-----------------------------------')
         self.trajectory = LineTrajectory("/planned_trajectory")
         u,v = self.mapToPixelCoords(self.goal_pos_irl[0],self.goal_pos_irl[1])
         self.goal_pos_map = (u,v) # no theta?
+        print('set new goal:'+str(self.goal_pos_map))
 
 
         self.plan_path((self.start_pos[0],self.start_pos[1]),self.goal_pos_map,self.map)
 
-
-
-    def AStarWithExpandedList(self,map,S,G):
-            
-            def computeH(u,v): # NOTE: currently this does it in pixel frame
-                return self.eucDist((u,v),(self.goal_pos_map[0],self.goal_pos_map[1]))
-            
-            def getChildren(i,j):
-                return [(i+1,j),(i-1,j),(i,j+1),(i,j-1),(i+1,j+1),(i-1,j-1),(i+1,j-1),(i-1,j+1)]
-
-            """
-            A* with an expanded list, assumes consistent heuristic to be correct
-            """
-
-            parent = {S:None}
-            expanded = set()
-            Q = [(computeH(S[0],S[1]),(0,S))] # (cost_to_come+cost_incurred,(cost_incurred, head))
-            heapq.heapify(Q)
-            print('planning path.......')
-            while Q:
-                _, N = heapq.heappop(Q)
-                head = N[1]; costIncurred = N[0]
-                if head == G:
-                    #get path from parent dictionary:
-                    curr = G
-                    path = [G]
-                    while parent[curr] != None:
-                        path.append(parent[curr])
-                        curr = parent[curr]
-                    path.reverse()
-                    print(path)
-                    return costIncurred, path
-                
-                elif head in expanded:
-                    continue
-                else:
-                    expanded.add(head) # NOTE: check!
-                    children = getChildren(head[0],head[1])
-                    for child in children:
-                        if child not in expanded:
-                            try:
-                                val = map[child[1]][child[0]]
-                                if val == 0:
-                                    parent[child] = head
-                                    costToChild = self.eucDist(head,child) + costIncurred
-                                    heapq.heappush(Q,(costToChild+computeH(child[0],child[1]),(costToChild,child)))
-                                    #heapq.heappush(Q,(costToChild,(costToChild,child)))
-                                else:
-                                    expanded.add(child)
-                            except: # out of bounds
-                                continue             
-            return None, None
 
     def AStarWithExpandedListPartialPaths(self,map,S,G):
             
@@ -181,11 +126,6 @@ class PathPlan(object):
             
             def getChildren(i,j):
                 return [(i+1,j),(i-1,j),(i,j+1),(i,j-1),(i+1,j+1),(i-1,j-1),(i+1,j-1),(i-1,j+1)]
-                #return [(i+1,j),(i-1,j),(i,j+1),(i,j-1),(i+1,j+1),(i-1,j-1),(i+1,j-1),(i-1,j+1),(i+2,j),(i-2,j),(i,j+2),(i,j-2),(i+2,j+2),(i-2,j-2),(i+2,j-2),(i-2,j+2),(i+2,j-1),(i-2,j+1),(i+2,j+1),(i-2,j-1),(i+1,j+2),(i-1,j-2),(i+1,j-2),(i-1,j+2)]
-
-            """
-            A* with an expanded list, assumes consistent heuristic to be correct
-            """
 
             expanded = set()
             Q = [(computeH(S[0],S[1]),(0,[S]))] # (cost_to_come+cost_incurred,(cost_incurred, head))
@@ -204,13 +144,11 @@ class PathPlan(object):
                     for child in children:
                         if child not in expanded:
                             try:
-                                val = map[child[1]][child[0]]
-                                if val == 0:
+                                if map[child[1]][child[0]] == 0:
                                     new_partial = partial_path[:]
                                     new_partial.append(child)
                                     costToChild = self.eucDist(head,child) + costIncurred
                                     heapq.heappush(Q,(costToChild+computeH(child[0],child[1]),(costToChild,new_partial)))
-                                    #heapq.heappush(Q,(costToChild,(costToChild,child)))
                                 else:
                                     expanded.add(child)
                             except: # out of bounds
@@ -227,9 +165,7 @@ class PathPlan(object):
             for node in partialPath:
                 point = Point()
                 x1,y1 = np.round(self.pixelToMapCoords(node[0],node[1]),decimals =2)
-                point.x = x1
-                point.y = y1
-                point.z = 0.0
+                point.x = x1; point.y = y1; point.z = 0.0
                 self.trajectory.addPoint(point)
 
             # publish trajectory
