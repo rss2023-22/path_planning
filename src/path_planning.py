f41e3a2e18ff83e5e4534b3b82a9aa6b5dfe223a
brrich <brrich@mit.edu>
2023-04-13 03:09:41 +0000

    testing gradescop

--- src/path_planning.py
+++ src/path_planning.py
@@ -11,7 +11,7 @@ import tf
 from utils import LineTrajectory
 from graph_class import Graph
 import heapq
-from skimage import morphology
+#from skimage import morphology
 
 #### NOTE: later on we should try dublin curves, nice package in the git for this...
 # roslaunch racecar_simulator simulate.launch
@@ -31,6 +31,7 @@ class PathPlan(object):
         self.map_coords = None
         self.height = None
         self.width = None
+        self.start_time = None
 
         self.odom_topic = rospy.get_param("~odom_topic")
         self.odom_topic = '/odom' # FOR TESTING ONLY - on real car, change to listen to localization shit... is this right?
@@ -67,9 +68,9 @@ class PathPlan(object):
         self.map_res = msg.info.resolution
         map = np.array(list(msg.data))
         map = np.reshape(map,(msg.info.height, msg.info.width)) # convert from row-major order
-        z = morphology.disk(10)
-        map = morphology.erosion(map,z)
-        print('eroded')
+        #z = morphology.disk(10)
+        #map = morphology.erosion(map,z)
+        
         height = np.shape(map)[0]; self.height = height
         width = np.shape(map)[1]; self.width = width
         self.heuristic = np.zeros((height,width))
@@ -79,35 +80,38 @@ class PathPlan(object):
         rot_matrix[0][3] = msg.info.origin.position.x; rot_matrix[1][3] = msg.info.origin.position.y; rot_matrix[2][3] = msg.info.origin.position.z
         rot_alt = [rot_matrix[0][0:3],rot_matrix[1][0:3],rot_matrix[2][0:3]]
         self.rot_matrix = rot_matrix; self.rot_alt = rot_alt; self.rot_back = np.linalg.inv(self.rot_matrix); self.rot_back_alt = np.linalg.inv(self.rot_alt)
- 
         self.map = map
+        print('MAP INITIATED')
+        print('-----------------------------------')
 
     def initial_pose_cb(self,data):
-        x = data.pose.pose.position.x
-        y = data.pose.pose.position.y
-        u,v = self.mapToPixelCoords(x,y)
-        print('new start')
+        u,v = self.mapToPixelCoords(data.pose.pose.position.x,data.pose.pose.position.y)
+        print('new start' + str(u,v))
+        print('-----------------------------------')
         self.start_pos = (u,v)
 
     def odom_cb(self, data): # sets the curren position of the car... currently green arrow doesn't work?? same fix as in last lab tho
-        # print('HERE')
-        # now_odom = np.array([data.twist.twist.linear.x, data.twist.twist.linear.y, data.twist.twist.angular.z])
-        # if self.rot_alt != None:
-        #     u,v = self.mapToPixelCoords(data.twist.twist.linear.x,data.twist.twist.linear.y)
-        #     self.start_pos = (u,v)
-        pass
+       # print('HERE')
+        now_odom = np.array([data.twist.twist.linear.x, data.twist.twist.linear.y, data.twist.twist.angular.z])
+        if self.rot_alt != None:
+            u,v = self.mapToPixelCoords(data.twist.twist.linear.x,data.twist.twist.linear.y)
+            self.start_pos = (u,v)
+            print('new start' + str((u,v)))
+            print('-----------------------------------')
+        #pass
 
 
     def goal_cb(self, data):
         theta = 2*np.arctan2(data.pose.orientation.z,data.pose.orientation.w)
         self.goal_pos_irl = (data.pose.position.x,data.pose.position.y,theta)
         print('set new goal:'+str(self.goal_pos_irl))
+        print('-----------------------------------')
         self.trajectory = LineTrajectory("/planned_trajectory")
         u,v = self.mapToPixelCoords(self.goal_pos_irl[0],self.goal_pos_irl[1])
         self.goal_pos_map = (u,v) # no theta?
 
 
-        self.plan_path((self.start_pos[0],self.start_pos[1]),self.goal_pos_map,self.map[:])
+        self.plan_path((self.start_pos[0],self.start_pos[1]),self.goal_pos_map,self.map)
 
 
 
@@ -118,7 +122,6 @@ class PathPlan(object):
             
             def getChildren(i,j):
                 return [(i+1,j),(i-1,j),(i,j+1),(i,j-1),(i+1,j+1),(i-1,j-1),(i+1,j-1),(i-1,j+1)]
-                #return [(i+1,j),(i-1,j),(i,j+1),(i,j-1),(i+1,j+1),(i-1,j-1),(i+1,j-1),(i-1,j+1),(i+2,j),(i-2,j),(i,j+2),(i,j-2),(i+2,j+2),(i-2,j-2),(i+2,j-2),(i-2,j+2),(i+2,j-1),(i-2,j+1),(i+2,j+1),(i-2,j-1),(i+1,j+2),(i-1,j-2),(i+1,j-2),(i-1,j+2)]
 
             """
             A* with an expanded list, assumes consistent heuristic to be correct
@@ -179,7 +182,6 @@ class PathPlan(object):
             expanded = set()
             Q = [(computeH(S[0],S[1]),(0,[S]))] # (cost_to_come+cost_incurred,(cost_incurred, head))
             heapq.heapify(Q)
-            print('planning path.......')
             while Q:
                 _, N = heapq.heappop(Q)
                 partial_path = N[1]; costIncurred = N[0]
@@ -208,19 +210,18 @@ class PathPlan(object):
             return None, None
 
     def plan_path(self, start_point, end_point, map):
-        # NOTE: start AND end are in pixel coords
-        # NOTE: could be better to store map in irl coords rather than pixel coords to avoid transforms during A*!
-
+        print('planning path.......')
+        self.start_time = rospy.get_time()
         _, partialPath = self.AStarWithExpandedListPartialPaths(map,start_point,end_point)
+        print('finished planning path, time = '+str(rospy.get_time()-self.start_time))
         
         if partialPath is not None:
-            # NOTE: changed utils.py to if True! should prob change back
             for node in partialPath:
                 point = Point()
                 x1,y1 = np.round(self.pixelToMapCoords(node[0],node[1]),decimals =2)
                 point.x = x1
                 point.y = y1
-                point.z = 100.0
+                point.z = 0.0
                 self.trajectory.addPoint(point)
 
             # publish trajectory
